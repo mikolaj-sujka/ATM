43de5e14d98eb6bc56f40d7989ed789f7f33e86d
Mikolaj Sujka <mikolajsujka@gmail.com>
2021-07-13 17:52:12 +0200

    Revert "Vsc git testing"
    This reverts commit 84cc17031cba522829961304bf32ba8154bf59c1.

--- Program.cs
+++ Program.cs
@@ -4,10 +4,9 @@ namespace ATM
 {
     class Program
     {
-        private static int a = 5;
         static void Main(string[] args)
         {
-            Console.WriteLine(Program.a);
+            Console.WriteLine("Hello World!");
         }
     }
 }
